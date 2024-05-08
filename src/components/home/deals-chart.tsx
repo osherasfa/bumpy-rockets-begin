99817a9dbac0119cfd1c0f1f8bb4b2448d7a4f1d
osherasfa <osherasfa@gmail.com>
2024-05-08 07:23:22 +0300

    1.0.3v

---
+++ src/components/home/deals-chart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DealsChart
+ = () => {
+  return (
+    <div>
+      DealsChart
+    </div>
+  )
+}
+
+export default DealsChart
---
+++ src/components/home/upcoming-events.tsx
@@ -0,0 +1,16 @@
+import { Card } from 'antd'
+import React from 'react'
+
+const UpcomingEvents = () => {
+  return (
+    <Card 
+      style={{ height: '100%' }}
+      headStyle={{ padding: '8px 16px' }}
+      bodyStyle={{ padding: '0 1rem' }}
+    >
+      UpcomingEvents
+    </Card>
+  )
+}
+
+export default UpcomingEvents
